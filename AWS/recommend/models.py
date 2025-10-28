f8d241d156b9e2c5eeb528ee17bf5c0bcec662f7
s4ngmin-9 <s4ngmin9@naver.com>
2025-10-28 16:56:06 +0900

    ✨ feat: recommend 모델 정ì

--- AWS/recommend/models.py
+++ AWS/recommend/models.py
@@ -1,6 +1,8 @@
 from django.db import models
+
 from AWS.users.models import User
 from AWS.weather.models import WeatherData
+
 
 class AIModelSetting(models.Model):
     name = models.CharField(max_length=150)
