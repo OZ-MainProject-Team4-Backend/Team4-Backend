9e451c68f6a28203256166055dc7ab7af230bf4e
s4ngmin-9 <s4ngmin9@naver.com>
2025-10-28 17:10:37 +0900

    ✨ feat: OutfitRecommendation 시리얼라이저 추가

---
+++ AWS/recommend/serializers.py
@@ -0,0 +1,11 @@
+from rest_framework import serializers
+from .models import OutfitRecommendation
+
+class OutfitRecommendationSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = OutfitRecommendation
+        fields = [
+            "id", "rec_1", "rec_2", "rec_3",
+            "explanation", "image_url",
+            "is_bookmarked", "created_at"
+        ]
