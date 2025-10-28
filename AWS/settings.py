050949190a2551bd0dd429aaa6e9d3d6f5bf696f
moxopills <moxopills@gmail.com>
2025-10-28 13:55:15 +0900

    pytest,pytest-cov제거

--- AWS/settings.py
+++ AWS/settings.py
@@ -39,9 +39,6 @@ INSTALLED_APPS = [
     "django.contrib.sessions",
     "django.contrib.messages",
     "django.contrib.staticfiles",
-    # test
-    "pytest",
-    "pytest-cov",
     # users
     "AWS.users",
     # JWT
