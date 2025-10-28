609ad530b9cc1601beb6d5c956f3bcfb802703f2
romiii35 <romiii9735@gmail.com>
2025-10-28 16:14:44 +0900

    fix: diary 앱 모델 추가(mypy)

--- AWS/settings.py
+++ AWS/settings.py
@@ -43,7 +43,8 @@ INSTALLED_APPS = [
     "AWS.users",
     "AWS.weather",
     "AWS.recommend",
-    "AWS.diary"
+    "AWS.diary",
+
     # JWT
     "rest_framework",
     "rest_framework_simplejwt",
